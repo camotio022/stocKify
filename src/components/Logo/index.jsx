c88da73860f0770b461fd97de20fd8777c5f1ca4
camotio022 <123275976+camotio022@users.noreply.github.com>
2026-01-22 01:37:56 -0300

    novos estilos de css

--- src/components/Logo/index.jsx
+++ src/components/Logo/index.jsx
@@ -1,26 +1,34 @@
 import { Link, Stack } from "@mui/material"
-import Image from '../../images/layout/feture.png'
-import Imag3 from '../../images/layout/feture.png'
+import Image from '../../images/layout/captura.png'
+import Imag3 from '../../images/layout/captura.png'
 export const LogoMainLayout = ({ image, text, black, top }) => {
     return (
         <Stack sx={{
             display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            flexDirection: 'column',
+            alignItems: 'flex-start',
+            justifyContent: 'flex-start',
+            flexDirection: 'row',
             marginTop: top ? '0px' : '12px',
-            width: '12vw',
-            height: '80px',
-
+            width: '100%',
+            maxWidth: '180px',
+            height: 'auto',
         }}>
             <Link sx={{
-                minWidth: '90px',
-                height: '80px'
+                width: '90%',
+                height: 'auto',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                transition: 'all 0.4s cubic-bezier(0.4, 0, 0.2, 1)',
+                '&:hover': {
+                    transform: 'scale(1.05)',
+                }
             }} href={'/'}>
                 <img
                     style={{
                         width: '100%',
-                        height: '100%'
+                        height: 'auto',
+
                     }}
                     src={(black ? Imag3 : Image) || image}
                     alt="imagem da logo da junta mais!" />
